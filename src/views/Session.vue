d3a6477c9f47a0dcd6ca830d79baa7842e640414
Ignacio Palomar <ignaciopalomarchacon@gmail.com>
2025-06-16 13:37:06 +0200

    asdasdad

--- src/views/Session.vue
+++ src/views/Session.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="register-container">
     <!-- Formulario de registro -->
-    <article class="form-box">
+    <article class="form-box  mx-3 mx-md-0">
       <h2>Inicio de Sesión</h2>
       <form @submit.prevent="handleSubmit">
         <!-- Campo de correo electrónico -->
