cd8312c0bf4e0fb13d37eb3dadf0b4758fd19f74
Ignacio Palomar <ignaciopalomarchacon@gmail.com>
2025-06-17 12:14:12 +0200

    tipografia

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/mi-frontend/',  // Cambia "mi-repo" por el nombre real de tu repositorio en GitHub
+  base: '/mi-frontend/',  
 })
